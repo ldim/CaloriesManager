40768f74aa0f1cb48c67b39677b72cda8cba8556
ldim <l_dim@mail.ua>
2018-03-18 15:20:55 +0200

    5 8 spring cache

--- src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserServiceImpl.java
@@ -1,6 +1,6 @@
 package ru.javawebinar.topjava.service;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
